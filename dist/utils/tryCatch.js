2b52946c126216928ee1cec484e6de43a7599193
niteshgiri-7 <niteshgiri1717@gmail.com>
2025-02-27 23:30:52 +0545

    removed:delete photo on error functions cz file is no longer being saved in disk

--- dist/utils/tryCatch.js
+++ dist/utils/tryCatch.js
@@ -1,13 +1,9 @@
-import deletePhotoOnError from "./deletePhoto.js";
 export const TryCatch = (func) => {
     return async function (req, res, next) {
         try {
             await func(req, res, next);
         }
         catch (error) {
-            if (req?.file?.path) {
-                deletePhotoOnError(req.file.path);
-            }
             next(error);
         }
     };
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import { getAllCustomers, signUp } from "../controllers/userControl.js";
-import { singleUpload } from "../middlewares/multerUploadMiddleware.js";
 import { authenticateUser, ensureAdminOnlyAccess } from "../middlewares/auth.js";
+import { uploadImageViaMulter } from "../middlewares/multerUploadMiddleware.js";
+import uploadToCloudinary from "../middlewares/uploadToCloudinary.js";
 
  const userRouter = express.Router();
 
 //  .../api/v1/user is the base url
 
-userRouter.post("/signUp",singleUpload,signUp);
+userRouter.post("/signUp",uploadImageViaMulter,uploadToCloudinary,signUp);
 
 
 
